96f811bab94557a7931db24d6997ccd705c561b1
Jamie Mansfield <jamie.mansfield.jm@gmail.com>
2016-05-03 03:58:31 +0100

    Switch to SCSS (#64)
    * Begin transition to scss
    
    * Port sponsors to scss
    
    * Port base css to sass
    
    * Port the header and footer among other things
    
    * Finish the scss port
    
    * Fix sponsors again

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,24 @@ var gulp = require('gulp'),
     uglify = require('gulp-uglify');
 
 gulp.task('watch', ['build'], function () {
-    gulp.watch('./public/assets/sass/**/*.sass', ['build']);
-    gulp.watch('./public/assets/sass/**/*.scss', ['build']);
+    gulp.watch('./public/assets/scss/**/*.scss', ['build']);
 
     gulp.watch('./public/assets/js/**/*.js', ['build']);
 });
 
 gulp.task('sass', function () {
-    return gulp.src('./public/assets/sass/spongehome.sass')
+    return gulp.src('./public/assets/scss/spongehome.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./public/assets/css'));
 });
 
 gulp.task('js', function () {
-    return gulp.src('./public/assets/js/**/*.js')
+    gulp.src('./public/assets/js/jquery.truncate.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./public/assets/js'));
+
+    return gulp.src('./public/assets/js/spongehome.js')
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./public/assets/js'));
