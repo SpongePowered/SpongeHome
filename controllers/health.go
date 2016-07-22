f983ebcc1d1542f9c744fe7e1941e05ad36e911f
Progwml6 <progwml6@gmail.com>
2016-07-22 19:29:00 -0400

    remove debug code, and reformat paramMap

--- controllers/health.go
+++ controllers/health.go
@@ -8,10 +8,14 @@ import (
 )
 
 func GetHealth(ctx *macaron.Context) {
-	paramMap := map[string]string{"BUILD_NUMBER": os.Getenv("BUILD_NUMBER"), "GIT_BRANCH": os.Getenv("GIT_BRANCH"), "GIT_COMMIT": os.Getenv("GIT_COMMIT"), "JOB_NAME": os.Getenv("JOB_NAME"), "BUILD_TAG": os.Getenv("BUILD_TAG"), "SPONGE_ENV": os.Getenv("SPONGE_ENV"), "SERVICE": "SpongeHome"}
-	jsonMarshalled, _ := json.Marshal(paramMap)
-	jsonStr := string(jsonMarshalled)
-	print(jsonStr)
-	//ctx.JSON(http.StatusOK, &jsonStr)
+	paramMap := map[string]string{
+		"BUILD_NUMBER": os.Getenv("BUILD_NUMBER"),
+		"GIT_BRANCH":   os.Getenv("GIT_BRANCH"),
+		"GIT_COMMIT":   os.Getenv("GIT_COMMIT"),
+		"JOB_NAME":     os.Getenv("JOB_NAME"),
+		"BUILD_TAG":    os.Getenv("BUILD_TAG"),
+		"SPONGE_ENV":   os.Getenv("SPONGE_ENV"),
+		"SERVICE":      "SpongeHome",
+	}
 	ctx.JSON(http.StatusOK, &paramMap)
 }
