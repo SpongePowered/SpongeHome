ffe541832dc5ef5f100728d37b7111640263368c
Zach <progwml6@gmail.com>
2016-07-22 20:29:01 -0400

    add fastly cache clearing & statusz endpoint closes #74 (#99)
    * add environment variables to docker for git/jenkins info and fastly keys
    
    * WIP add fastly cache clearing code
    
    * move fastly cache clear to a goroutine so that it runs after the web server has started up
    
    * use environment variable to determine if prod/staging
    
    * remove test variables from dockerfile
    
    *  remove FASTLY_KEY from build.sh add /health endpoint that adds info to identify what is running on this service
    
    * remove debug code, and reformat paramMap
    
    * move from health to statusz
    
    * additional cleanup of fastly cache clear code
    
    * additional go styling fixes
    
    * move json data from a variable to the method call itself
    
    * more style fixes, return in some cases where there would be errors
    
    * another round of style fixes and adding return statements

--- spongehome.go
+++ spongehome.go
@@ -26,23 +26,69 @@
 package main
 
 import (
-    "github.com/SpongePowered/SpongeHome/controllers"
-    "github.com/go-macaron/pongo2"
-    "gopkg.in/macaron.v1"
+	"fmt"
+	"github.com/SpongePowered/SpongeHome/controllers"
+	"github.com/go-macaron/pongo2"
+	"github.com/sethvargo/go-fastly"
+	"gopkg.in/macaron.v1"
+	"os"
+	"time"
 )
 
+func clearFastly() {
+	// Clear Fastly cache
+	if os.Getenv("FASTLY_KEY") != "" {
+		time.Sleep(10 * time.Second) // Let the http server startup
+		fmt.Println("starting fastly client")
+		client, err := fastly.NewClient(os.Getenv("FASTLY_KEY"))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		services, err := client.ListServices(&fastly.ListServicesInput{})
+		fastlyService := ""
+		if os.Getenv("SPONGE_ENV") == "prod" {
+			fastlyService = "www.spongepowered.org"
+		} else if os.Getenv("SPONGE_ENV") == "staging" {
+			fastlyService = "www-staging.spongepowered.org"
+		} else if os.Getenv("SPONGE_ENV") == "" {
+			fmt.Println("SPONGE_ENV is not set")
+			return
+		}
+		for _, svc := range services {
+			if svc.Name == fastlyService {
+				_, err := client.PurgeAll(&fastly.PurgeAllInput{
+					Service: svc.ID,
+					Soft:    false,
+				})
+				if err != nil {
+					fmt.Println("fastly cache purge failed")
+					fmt.Println(err)
+					return
+				} else {
+					fmt.Println("found match and purged")
+				}
+			}
+		}
+		fmt.Println("Finished clearing Fastly cache")
+	}
+}
+
 func main() {
-    // Initialise Macaron
-    m := macaron.Classic()
-    m.Use(pongo2.Pongoer())
+	// Initialise Macaron
+	m := macaron.Classic()
+	m.Use(pongo2.Pongoer())
 
-    // Routes
-    m.Get("/", controllers.GetHomepage)
-    m.Get("/sponsors", controllers.GetSponsors)
-    m.Get("/chat", controllers.GetChat)
+	// Routes
+	m.Get("/", controllers.GetHomepage)
+	m.Get("/sponsors", controllers.GetSponsors)
+	m.Get("/chat", controllers.GetChat)
+	m.Get("/statusz", controllers.GetStatusz)
+	m.Get("/announcements.json", controllers.GetAnnouncements)
 
-    m.Get("/announcements.json", controllers.GetAnnouncements)
+	go clearFastly()
 
-    // Run SpongeHome
-    m.Run()
+	// Run SpongeHome
+	m.Run()
+
 }
