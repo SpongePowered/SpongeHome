bd74790d320709d1e114e701b57d6726d841436f
Jamie Mansfield <dev@jamierocks.uk>
2016-10-22 00:19:14 +0100

    Remove generated assets and move source to src directory

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,28 @@
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
+var gulp   = require('gulp'),
+    sass   = require('gulp-sass'),
     cssmin = require('gulp-cssmin'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify');
 
 gulp.task('watch', ['build'], function () {
-    gulp.watch('./public/assets/scss/**/*.scss', ['build']);
-
-    gulp.watch('./public/assets/js/**/*.js', ['build']);
+    gulp.watch('./src/scss/**/*.scss', ['scss']);
+    gulp.watch('./src/js/**/*.js',     ['js']);
 });
 
 gulp.task('scss', function () {
-    return gulp.src('./public/assets/scss/spongehome.scss')
+    return gulp.src('./src/scss/spongehome.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/assets/css'));
-});
-
-gulp.task('js', function () {
-    gulp.src('./public/assets/js/jquery.truncate.js')
-        .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./public/assets/js'));
-
-    return gulp.src('./public/assets/js/spongehome.js')
-        .pipe(uglify())
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest('./public/assets/js'));
-});
-
-gulp.task('build', ['scss', 'js'], function () {
-    return gulp.src('./public/assets/css/spongehome.css')
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./public/assets/css'));
 });
 
-gulp.task('default', ['build'], function () {
-
+gulp.task('js', function () {
+    return gulp.src('./src/js/*.js')
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('./public/assets/js'));
 });
+
+gulp.task('build', ['scss', 'js']);
+gulp.task('default', ['build']);
