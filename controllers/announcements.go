9436e657de74d3abc711653c031fe0d364ae1310
Minecrell <dev@minecrell.net>
2016-10-23 19:23:28 +0200

    Major cleanup
    - Reformat Go files
    - Delete a lot of unused CSS code
    - Update JS dependencies
    - Cleanup HTML

--- controllers/announcements.go
+++ controllers/announcements.go
@@ -26,113 +26,113 @@
 package controllers
 
 import (
-    "strconv"
-    "net/http"
-    "encoding/json"
-    "gopkg.in/macaron.v1"
+	"encoding/json"
+	"gopkg.in/macaron.v1"
+	"net/http"
+	"strconv"
 )
 
 type Topic struct {
-    ID int `json:"id"`
-    Title string `json:"title"`
-    Slug string `json:"slug"`
-    Archetype string `json:"archetype"`
+	ID        int    `json:"id"`
+	Title     string `json:"title"`
+	Slug      string `json:"slug"`
+	Archetype string `json:"archetype"`
 }
 
 type TopicList struct {
-    Topics []Topic `json:"topics"`
+	Topics []Topic `json:"topics"`
 }
 
 func (t *TopicList) GetRegularTopics() []Topic {
-    var topics []Topic = []Topic{}
+	var topics []Topic = []Topic{}
 
-    for _, topic := range t.Topics {
-        if topic.Archetype == "regular" {
-            topics = append(topics, topic)
-        }
-    }
+	for _, topic := range t.Topics {
+		if topic.Archetype == "regular" {
+			topics = append(topics, topic)
+		}
+	}
 
-    return topics
+	return topics
 }
 
 type Category struct {
-    TopicList TopicList `json:"topic_list"`
+	TopicList TopicList `json:"topic_list"`
 }
 
 type Post struct {
-    Cooked string `json:"cooked"`
+	Cooked string `json:"cooked"`
 }
 
 type PostStream struct {
-    Posts []Post `json:"posts"`
+	Posts []Post `json:"posts"`
 }
 
 type TopicResponse struct {
-    PostStream PostStream `json:"post_stream"`
+	PostStream PostStream `json:"post_stream"`
 }
 
 type AnnouncementView struct {
-    First Announcement `json:"first"`
-    Second Announcement `json:"second"`
+	First  Announcement `json:"first"`
+	Second Announcement `json:"second"`
 }
 
 type Announcement struct {
-    Title string `json:"title"`
-    Content string `json:"content"`
-    Link string `json:"link"`
+	Title   string `json:"title"`
+	Content string `json:"content"`
+	Link    string `json:"link"`
 }
 
 func GetAnnouncements(ctx *macaron.Context) {
-    var res Category
+	var res Category
 
-    r, err := http.Get("https://forums.spongepowered.org/c/announcements.json?order=created")
-    if err != nil {
-        ctx.Error(http.StatusInternalServerError, "Can't access announcements!")
-        return
-    }
+	r, err := http.Get("https://forums.spongepowered.org/c/announcements.json?order=created")
+	if err != nil {
+		ctx.Error(http.StatusInternalServerError, "Can't access announcements!")
+		return
+	}
 
-    err = json.NewDecoder(r.Body).Decode(&res)
-    if err != nil {
-        ctx.Error(http.StatusInternalServerError, "Can't access announcements!")
-        return
-    }
+	err = json.NewDecoder(r.Body).Decode(&res)
+	if err != nil {
+		ctx.Error(http.StatusInternalServerError, "Can't access announcements!")
+		return
+	}
 
-    var topics []Topic = res.TopicList.GetRegularTopics()
+	var topics []Topic = res.TopicList.GetRegularTopics()
 
-    first, err := getAnnouncement(topics[0])
-    if err != nil {
-        ctx.Error(http.StatusInternalServerError, "Can't access the first topic!")
-        return
-    }
+	first, err := getAnnouncement(topics[0])
+	if err != nil {
+		ctx.Error(http.StatusInternalServerError, "Can't access the first topic!")
+		return
+	}
 
-    second, err := getAnnouncement(topics[1])
-    if err != nil {
-        ctx.Error(http.StatusInternalServerError, "Can't access the second topic!")
-        return
-    }
+	second, err := getAnnouncement(topics[1])
+	if err != nil {
+		ctx.Error(http.StatusInternalServerError, "Can't access the second topic!")
+		return
+	}
 
-    ctx.JSON(http.StatusOK, &AnnouncementView{
-        First: first,
-        Second: second,
-    })
+	ctx.JSON(http.StatusOK, &AnnouncementView{
+		First:  first,
+		Second: second,
+	})
 }
 
 func getAnnouncement(topic Topic) (Announcement, error) {
-    var res TopicResponse
+	var res TopicResponse
 
-    r, err := http.Get("https://forums.spongepowered.org/t/" + strconv.Itoa(topic.ID) + ".json")
-    if err != nil {
-        return Announcement{}, err
-    }
+	r, err := http.Get("https://forums.spongepowered.org/t/" + strconv.Itoa(topic.ID) + ".json")
+	if err != nil {
+		return Announcement{}, err
+	}
 
-    err = json.NewDecoder(r.Body).Decode(&res)
-    if err != nil {
-        return Announcement{}, err
-    }
+	err = json.NewDecoder(r.Body).Decode(&res)
+	if err != nil {
+		return Announcement{}, err
+	}
 
-    return Announcement{
-        Title: topic.Title,
-        Content: res.PostStream.Posts[0].Cooked,
-        Link: "https://forums.spongepowered.org/t/" + topic.Slug,
-    }, nil
+	return Announcement{
+		Title:   topic.Title,
+		Content: res.PostStream.Posts[0].Cooked,
+		Link:    "https://forums.spongepowered.org/t/" + topic.Slug,
+	}, nil
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var gulp = require('gulp'),
+const
+    gulp = require('gulp'),
     util = require('gulp-util'),
     named = require('vinyl-named'),
     rename = require('gulp-rename'),
@@ -7,44 +8,45 @@ var gulp = require('gulp'),
     gulpWebpack = require('webpack-stream'),
 
     sass = require('gulp-sass'),
+
+    postcss = require('gulp-postcss'),
+    autoprefixer = require('autoprefixer'),
+
     uglify = require('gulp-uglify'),
     cleanCSS = require('gulp-clean-css');
 
-gulp.task('scss', function () {
-    return gulp.src('./src/scss/spongehome.scss')
+gulp.task('scss', () =>
+    gulp.src('./src/scss/spongehome.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(postcss([
+            autoprefixer()
+        ]))
         .pipe(gulp.dest('./public/assets/css'))
         .pipe(cleanCSS())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./public/assets/css'))
-});
+);
 
-gulp.task('js', function () {
-    return gulp.src('./src/js/static/*.js')
+gulp.task('js', ()  =>
+    gulp.src('./src/js/static/*.js')
         .pipe(gulp.dest('./public/assets/js'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./public/assets/js'))
-});
+);
 
-function runWebpack(env) {
-    return gulp.src([])
+const runWebpack = (env) =>
+    gulp.src([])
         .pipe(named())
         .pipe(gulpWebpack(require('./webpack.config.js')(env), webpack))
-        .pipe(gulp.dest('./public/assets/js'))
-}
+        .pipe(gulp.dest('./public/assets/js'));
 
-gulp.task('webpack-dev', function() {
-    return runWebpack('development')
-});
-
-gulp.task('webpack', ['webpack-dev'], function() {
-    return runWebpack('production')
-});
+gulp.task('webpack-dev', () => runWebpack('development'));
+gulp.task('webpack', ['webpack-dev'], () => runWebpack('production'));
 
 gulp.task('build', ['scss', 'js', 'webpack']);
 gulp.task('default', ['build']);
 
-gulp.task('watch', ['build'], function () {
-    gulp.watch('./src/**', ['build']);
-});
+gulp.task('watch', ['build'], () =>
+    gulp.watch('./src/**', ['build'])
+);
