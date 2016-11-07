5cdf306205bb998a1ce67850a139cc2cc4b135a8
Minecrell <dev@minecrell.net>
2016-11-07 16:13:17 +0100

    Register /statusz only when environment variable is set

--- controllers/statusz.go
+++ controllers/statusz.go
@@ -32,22 +32,38 @@ import (
 	"regexp"
 )
 
-var (
-	buildNameRe = regexp.MustCompile(`^(.+)-([0-9]+)$`)
-)
+const service = "SpongeHome"
 
-func GetStatusz(ctx *macaron.Context) {
-	buildNamePieces := buildNameRe.FindStringSubmatch(os.Getenv("OPENSHIFT_BUILD_NAME"))
+func readStatus() interface{} {
+	buildName := os.Getenv("OPENSHIFT_BUILD_NAME")
+	if buildName == "" {
+		return nil
+	}
+
+	buildNameRe := regexp.MustCompile(`^(.+)-([0-9]+)$`)
+	buildNamePieces := buildNameRe.FindStringSubmatch(buildName)
 	jobName := buildNamePieces[1]
 	buildNum := buildNamePieces[2]
 	buildTag := os.Getenv("OPENSHIFT_BUILD_NAMESPACE") + "/" + os.Getenv("OPENSHIFT_BUILD_NAME")
-	ctx.JSON(http.StatusOK, map[string]string{
+
+	return map[string]string{
 		"BUILD_NUMBER": buildNum,
 		"GIT_BRANCH":   os.Getenv("OPENSHIFT_BUILD_REFERENCE"),
 		"GIT_COMMIT":   os.Getenv("OPENSHIFT_BUILD_COMMIT"),
 		"JOB_NAME":     jobName,
 		"BUILD_TAG":    buildTag,
 		"SPONGE_ENV":   os.Getenv("SPONGE_ENV"),
-		"SERVICE":      "SpongeHome",
-	})
+		"SERVICE":      service,
+	}
+}
+
+func StatuszHandler() macaron.Handler {
+	status := readStatus()
+	if status == nil {
+		return nil
+	}
+
+	return func(ctx macaron.Render) {
+		ctx.JSON(http.StatusOK, status)
+	}
 }
