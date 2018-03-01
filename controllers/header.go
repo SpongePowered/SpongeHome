c68c60c7421c7beb667cffa27c73146749aa359d
Minecrell <minecrell@minecrell.net>
2018-03-01 12:06:33 +0100

    controllers: Avoid adding cache control headers twice
    Fixes #141

--- controllers/header.go
+++ controllers/header.go
@@ -36,16 +36,16 @@ func AddHeaders(resp http.ResponseWriter) {
 	header := resp.Header()
 
 	// TODO: Needs more testing and a few changes to make it more restrictive
-	/*header.Add("Content-Security-Policy", "default-src 'self' https:; "+
+	/*header.Set("Content-Security-Policy", "default-src 'self' https:; "+
 	"style-src 'self' 'unsafe-inline' https:; "+
 	"script-src 'self' 'unsafe-eval' https://cdnjs.cloudflare.com https://www.google-analytics.com; "+
 	"frame-ancestors 'none'")*/
 
 	// Set cache headers only in production environment
 	if macaron.Env == macaron.PROD {
-		header.Add(cache.CacheControlHeader, cache.StaticContentOptions)
+		header.Set(cache.CacheControlHeader, cache.StaticContentOptions)
 
 		// Fastly cache header
-		header.Add(fastly.SurrogateControlHeader, cache.SurrogateStaticContentOptions)
+		header.Set(fastly.SurrogateControlHeader, cache.SurrogateStaticContentOptions)
 	}
 }
