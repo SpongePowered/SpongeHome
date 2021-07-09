1d70cd661e0b386acfe9c3eb41bc48648ec2d535
Felix Kleinekathoefer <git@felixoi.com>
2021-07-10 00:30:18 +0200

    Removed go from the project

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,5 @@
-/*
- * This file is part of SpongeHome, licensed under the MIT License (MIT).
- *
- * Copyright (c) SpongePowered <https://www.spongepowered.org>
- * Copyright (c) contributors
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 function addAnnouncement(announcement) {
-    var article = $('<article>').trunk8({
+    const article = $('<article>').trunk8({
         lines: 16,
         tooltip: false
     });
@@ -42,8 +17,31 @@ function addAnnouncement(announcement) {
     article.trunk8('update', announcement.content);
 }
 
-$.getJSON('/announcements.json', function(data) {
-    $('#announcements').empty();
-    addAnnouncement(data.first);
-    addAnnouncement(data.second);
-});
+const forumsBase = <%= config %>;
+const announcements = [];
+
+$.getJSON(forumsBase + '/c/announcements.json?order=created', function (data) {
+    const topics = data.topic_list.topics
+        .filter((t) => t.archetype === 'regular')
+        .splice(undefined, 2)
+
+    Promise.all(
+        topics.map((t) => {
+            return $.getJSON(forumsBase + '/t/' + t.id + '.json', function (topic) {
+                announcements.push({
+                    created_at: topic.created_at,
+                    title: topic.title,
+                    content: topic.post_stream.posts[0].cooked,
+                    url: forumsBase + '/t/' + topic.slug,
+                })
+            })
+        })
+    ).then(() => {
+        if (announcements.length > 0) {
+            $('#announcements').empty();
+            announcements.forEach(a => {
+                addAnnouncement(a);
+            })
+        }
+    })
+})
